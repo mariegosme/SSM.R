b50dbda7c01d0f0d0948ddafee4a6997d4e7ddb9
defranced <32944165+defranced@users.noreply.github.com>
2019-01-23 21:49:01 +0100

    Revert "Update functionsSSM.R"
    This reverts commit a06fc1138439bbbbe4e3331f56dedba0ccf3793f.

--- functionsSSM.R
+++ functionsSSM.R
@@ -11,51 +11,14 @@ fComputePAR<-function(globalradiation, CoefPAR=ALLPARAMETERS$pCoefPAR) {
   
 }
 
-fComputeTemp<-function(tasmax,tasmin) {
-  return(cTemp = (tasmax+tasmin)/2)
-
-}
-
-fDelta_thermal_unit<-function(pTbasdev,pTopt1dev,cCoefTemp=1,cWaterStressFactorDevelopment=1) {
-  return(cDeltaThermalUnit = (pTopt1dev - pTbasdev)*cCoefTemp)
-
-}
-
-fBiologicalDay<-function(cCoefTemp=1,cCoefPhotoPeriod=1,cWaterStressFactorDevelopment=1){
-  
-  return(cBiologicalDay=cCoefTemp*cCoefPhotoPeriod*cWaterStressFactorDevelopment*cCoefVernalization)
-}
-
-fComptuteCoefTemp=function(cTemp,pTbasdev,pTopt1dev,pTop2dev,pTlethaldev) {
-  if(cTemp <= pTbasdev |cTemp >= pTlethaldev){
-    cCoefTemp = 0 
-  }else if(cTemp > pTbasdev & cTemp < pTopt1dev){
-    cCoefTemp = (cTemp - pTbasdev) / (pTopt1dev - pTbasdev)
-  }else if(cTemp > pTopt2dev & cTemp < pTlethaldev){
-    cCoefTemp = ((pTlethaldev - cTemp) / (pTlethaldev - pTopt2dev))
-  }else{
-    cCoefTemp = 1
-  }
-
-  return(cCoefTemp)
-}
-
 #### definition of procedures (which update state variables)
 
 rUpdatePAR<-function(){
   print("updating PAR")
   
-  step<-length(ALLSIMULATEDDATA)   
+  step<-length(ALLSIMULATEDDATA)
   PAR<-fComputePAR(globalradiation=ALLSIMULATEDDATA[[step]]$iRSDS, CoefPAR=ALLPARAMETERS$pCoefPAR)
   ALLSIMULATEDDATA[[step]]$cPAR<<-PAR
-}
-
-rUpdateThermalUnite<-function(){
-  print("updating ThermalUnite")
-  
-  step<-length(ALLSIMULATEDDATA)  
-  ThermalUnite<-ALLSIMULATEDDATA[[step-1]]$sThermalUnite + fDelta_thermal_unit(pTbasdev,pTopt1dev,Tempovar$cCoefTemp)
-  ALLSIMULATEDDATA[[step]]$sThermalUnit<<-ThermalUnite
 }
 
 
