1fac8a951bf98af8bb31beb0a86c965f6e01ad73
Marie Gosme <user@SY-MARIEPORTABLE.local>
2020-02-29 12:58:58 +0100

    development version corrected
    I reverted the merge pull request because I wanted to do a rebase rather than a merge commit... but it didn't work: it reverted also the development version to the master state. Luckily I had made a local save. So now I commit eveything back to the master branch. Next time I'll try to select the good type of commit

--- HousekeepingFunctionsSSM.R
+++ HousekeepingFunctionsSSM.R
@@ -1,28 +1,112 @@
-fCreateDataFrame<-function(d, types, rows) {
-  df<-cbind(data.frame(iDate=d),
-            as.data.frame(lapply(types, function(x) return(do.call(x, list(length(rows)))))))
-  rownames(df)<-rows
+#fonctions that are not in the core of the SSM model, but are neccessary for SSM.R to run
+
+## special function that returns a logical vector indicating of the given process applies to each case (depending on crop present in the case and its phenology (and the filters in the crop parameters)) 
+applyfilters<-function(processname){
+  cultivars<-paste(ALLDAYDATA$sCrop, ALLDAYDATA$sCultivar, sep=".")
+  possiblecrops<- unique(cultivars)
+  evaluatecrop<-function(text, uniquecrop) {
+    possiblestages<-names(ALLCROPS[uniquecrop,"thresholds"][[1]])
+    numstages<-1:length(possiblestages); names(numstages)<-possiblestages
+    is.before<-function(stage, bd=-Inf, crop=uniquecrop) { #if bd is not provided, the stage is not included
+      if(!stage %in% possiblestages) stop(stage," used in ", processname, " filter for cultivar ", uniquecrop, " is not in the list of stages for that cultivar")
+      whichcases<- (paste(ALLDAYDATA$sCrop, ALLDAYDATA$sCultivar, sep=".")==uniquecrop)
+      currentstages<-numstages[ALLDAYDATA$sGrowthStage[whichcases]]
+      currentbd<-ALLDAYDATA$sBiologicalDay[whichcases]
+      targetstage<-which(possiblestages==stage)
+      compare<-currentstages==targetstage & currentbd<=bd | currentstages<targetstage
+      result<-rep(FALSE, nrow(ALLDAYDATA))
+      result[whichcases]<-compare
+      return(result)
+    }
+    is.after<-function(stage, bd=Inf) { #if bd is not provided, the stage is not included
+      if(!stage %in% possiblestages) stop(stage," used in ", processname, " filter for cultivar ", uniquecrop, " is not in the list of stages for that cultivar")
+      whichcases<- (paste(ALLDAYDATA$sCrop, ALLDAYDATA$sCultivar, sep=".")==uniquecrop)
+      currentstages<-numstages[ALLDAYDATA$sGrowthStage[whichcases]]
+      currentbd<-ALLDAYDATA$sBiologicalDay[whichcases]
+      targetstage<-which(possiblestages==stage)
+      compare<-currentstages==targetstage & currentbd>=bd | currentstages>targetstage
+      result<-rep(FALSE, nrow(ALLDAYDATA))
+      result[whichcases]<-compare
+      return(result)
+    }
+    result<-unname(eval(parse(text=text)))
+    return(result)
+  }
+  filtertexts<-ALLCROPS[possiblecrops, paste(processname, "filter", sep=".")]
+  if (any(is.null(filtertexts))) stop(
+    "process ", processname, " is not in the filter parameters of cultivars ", paste(
+     possiblecrops[is.null(filtertexts)], collapse=", "
+  ))
+  filters<-mapply(FUN=evaluatecrop, filtertexts, possiblecrops, SIMPLIFY=FALSE)
+  resultfilter<-mapply(FUN=any, filters)
+  if(any(is.na(resultfilter))) stop("Error in applyfilter for process", processname, ": it returned NAs")
+  return(resultfilter)
+}
+
+fCreateDay<-function(dateday){
+  types<-VARIABLEDEFINITIONS[VARIABLEDEFINITIONS$name!="iDate", "typeR"]
+  names(types)<-VARIABLEDEFINITIONS[VARIABLEDEFINITIONS$name!="iDate", "name"]
+  df<-cbind(data.frame(iDate=dateday),
+            as.data.frame(lapply(types, function(x) return(do.call(x, list(nrow(PARAMSIM$cases)))))))
+  rownames(df)<-rownames(PARAMSIM$cases)
+ #initialize all variables as their default values
+  df[,setdiff(names(df), "iDate")]<-lapply(setdiff(names(df), "iDate"), function(x) VARIABLEDEFINITIONS[x, "defaultInitialvalue"])
+  df[,names(types)[types=="numeric"]]<-lapply(df[,names(types)[types=="numeric"]],as.numeric)
   return(df)
 }
 
-
-rCreateDay<-function() { 
-  #add a new day to ALLSIMULATEDDATA
-  laststep<-length(ALLSIMULATEDDATA)
-  print(paste("creating day", laststep+1))
-  ALLSIMULATEDDATA <<- c(ALLSIMULATEDDATA, list(fCreateDataFrame(
-    d=ALLSIMULATEDDATA[[laststep]][,"iDate"]+1, 
-    types=PARAMSIM$types,
-    rows=rownames(PARAMSIM$cases))))
+#creation of day 0 
+rCreateDay0<-function() {
+  print("initializing the simulation (with crops because sowing hasnt been coded yet" )
+  if(is.null(PARAMSIM$simustart)) stop("you didn't define a starting date, use $setoptions to do it")
+  if (sum( !(VARIABLEDEFINITIONS[VARIABLEDEFINITIONS$name!="iDate", "typeR"]%in% c("numeric", "character")) )>0) stop("all variables should be either numeric or character, check variables", paste(names(types)[!types %in% c("numeric", "character")], collapse=","))
+  df<-fCreateDay(PARAMSIM$simustart-1)
+  #we start without crop (sowing in the future, harvest in the past)
+  #df$sLastSowing<-Inf 
+  #df$sLastHarvest<- (-Inf)
+  
+  #icicici : actually we initialize with a crop everywhere because crop management hasnt been coded yet
+  df$sLastSowing<-0 
+  df$sLastHarvest<- (-Inf)
+  df$sCrop<-c("WHEAT", "MAIZE", "Chickpea") #written as in the first line in the excel file
+  df$sCultivar<-c("Ble_Dur_1", "bidule", "Ghab2") 
+  df$sGrowthStage<-c("germination", "SOW", "SOW")
+  df$sDurationStage<-c(6, 3, 8.5)
+  df$sGrowthStageNumber<-1
+  df$sBiologicalDay<-0
+  df$sPlantdensity<-280
+  
+  
+  ALLSIMULATEDDATA<<-list(df) #list of data.frames from the previous timesteps (here: day 0)
   return()
 }
 
-rUpdateClimate<-function() {
-  print("updating climate")
-  step<-length(ALLSIMULATEDDATA)
-  date<-ALLSIMULATEDDATA[[step]][1,"iDate"]
-  dfclimate<-fGetClimateDay(date=date)
-  climatevar<-VARIABLEDEFINITIONS[VARIABLEDEFINITIONS$module=="climate",]
-  climatevariables<-climatevar[,"name"]
-  ALLSIMULATEDDATA[[step]][,climatevariables]<<-dfclimate[PARAMSIM$cases$climatename, climatevariables]
+
+# creates a new data.frame at the beginning of the day
+rCreateDay<-function() { 
+  #create frame of data of the day 
+  daybefore<-length(ALLSIMULATEDDATA)
+  print(paste("creating day", daybefore)) #because there is a day 0
+  dateday<-ALLSIMULATEDDATA[[daybefore]][,"iDate"]+1
+  df<-fCreateDay(dateday)
+  #initialize state variables to the state at preceding timestep
+  statevars<-names(df)[substr(x=colnames(df), start=1, stop=1)=="s"]
+  df[,statevars]<-ALLSIMULATEDDATA[[daybefore]][,statevars]
+  ALLDAYDATA<<-df
+  return()
 }
+
+#sets the parameters in ALLDAYDATA from the current crop in each case
+rSetParamsFromCrops<-function(){
+  df<-ALLDAYDATA[,VARIABLEDEFINITIONS[VARIABLEDEFINITIONS$typeinthemodel =="CropParameter", "name"]]
+  cultivars<-paste(ALLDAYDATA$sCrop, ALLDAYDATA$sCultivar, sep=".")
+  possiblecrops<-unique(cultivars)
+  missing<-setdiff(names(df), names(ALLCROPS))
+  if(length(missing)>0) warning("the following parameters are mising from crop parameters: ", paste(missing, collapse=", "))
+  paramstobechanged<-intersect(names(df), names(ALLCROPS))
+  df[, paramstobechanged]<-ALLCROPS[cultivars, paramstobechanged]
+  ALLDAYDATA[,names(df)]<<-df
+  return()
+}
+
+  
--- externalFilesReadingSSM.R
+++ externalFilesReadingSSM.R
@@ -5,12 +5,12 @@
 
 fGetClimateDay<-function(date){ #returns a data.frame with the date, climate name and climate variables in columns, and possibly several rows, named by the climate name
   if (PARAMSIM$climateformat %in% c("standardSSM")) {
-    selectday<-ALLCLIMATE[ALLCLIMATE$date==date,]
+    selectday<-ALLCLIMATES[ALLCLIMATES$date==date,]
     rownames(selectday)<-selectday$climatename
     return(selectday)
   } else if (PARAMSIM$climateformat=="netCDF") {
-    #do something that returns a data.frame with colums 
-    #date, 
+    #do something that returns a data.frame with colums
+    #date,
     #optionnally one or more columns to define the cases (e.g. location, climate model etc...)
     #and all necessary climate variables (named as in the variable definition file)
     #and rownames should be climate names as used in the simulation options (PARAMSIM$case$climatename)
@@ -21,15 +21,17 @@ fGetClimateDay<-function(date){ #returns a data.frame with the date, climate nam
 eReadInInputs<-function(){
   eReadClimate()
   eReadSoil()
+  eReadCrop()
+  eReadManagement()
 }
 
-eReadClimate<-function(){ 
+eReadClimate<-function(){
   if (PARAMSIM$climateformat=="standardSSM") { #if climate read from excel, read file only once and load it in the workspace
-    climatevar<-VARIABLEDEFINITIONS[VARIABLEDEFINITIONS$module=="climate",]
+    climatevar<-VARIABLEDEFINITIONS[VARIABLEDEFINITIONS$module=="weather" & VARIABLEDEFINITIONS$typeinthemodel=="input",]
     newnames<-climatevar$name ; names(newnames)<-climatevar$translationSSM
     pathtoExcel<-normalizePath(paste(PARAMSIM$directory, "/input/climates.xlsx", sep=""))
     locations<-getSheetNames(pathtoExcel)
-    ALLCLIMATE<<-data.frame()
+    ALLCLIMATES<<-data.frame()
     for (sheet in locations) {
       onesheet<-read.xlsx(pathtoExcel, sheet=sheet, colNames=TRUE, startRow =10)
       #transforms year-DOY into date
@@ -42,14 +44,15 @@ eReadClimate<-function(){
       missingvariable<-setdiff(newnames, names(onesheet))
       if (length(missingvariable)>0) warning(paste("Sheet", sheet, "in file", pathtoExcel, "misses variables", paste(missingvariable, collapse=",")))
       onesheet$climatename<-sheet
-      ALLCLIMATE<<-rbind(ALLCLIMATE, onesheet)
+      ALLCLIMATES<<-rbind(ALLCLIMATES, onesheet)
     }
   } else if (PARAMSIM$climateformat=="netCDF")  {
+    stop("netCDF format not yet supported for climate")
     #do something, e.g. just open the metadata
-  } 
+  }
 } #end read climate
 
-eReadSoil<-function(){ 
+eReadSoil<-function(){
   if (PARAMSIM$soilformat=="standardSSM") { #if soil read from excel, read file only once and load it in the workspace
     pathtoExcel<-normalizePath(paste(PARAMSIM$directory, "/input/soils.xlsx", sep=""))
     locations<-getSheetNames(pathtoExcel)
@@ -57,6 +60,173 @@ eReadSoil<-function(){
     for (sheet in locations) {
       #to do: decide how to tracks soil parameters and variables for each layer
     }
-  } else if (PARAMSIM$soilformat=="")  {
-  } 
+  } else  {
+    stop("Only standard SSM format is supported for soil data")
+  }
 } #end read soil
+
+eReadCrop<-function(){
+  ALLCROPS<<-eReadExcelCropParameters(xlsxfile=normalizePath(paste(PARAMSIM$directory, "/input/crops.xlsx", sep="")),
+                           allvariablesfile="allvariables.xlsx")
+}#end read crops
+
+# eReadCrops_OLD<-function(){
+#   if (PARAMSIM$cropformat=="standardSSM") { #if soil read from excel, read file only once and load it in the workspace
+#     pathtoExcel<-normalizePath(paste(PARAMSIM$directory, "/input/crops.xlsx", sep=""))
+#     availablecrops<-getSheetNames(pathtoExcel)
+#     ALLCROPS<<-list()
+#     for (crop in availablecrops) {
+#       dfcrop<-read.xlsx(pathtoExcel, sheet=crop, colNames=TRUE, startRow =2) #first column = parameter name, second = units (sometimes), other columns: cultivars
+#       rownames(dfcrop)<-dfcrop$name
+#       for (cultivar in names(dfcrop)[-(1:3)]) {
+#         pasflat<-is.na(as.numeric(dfcrop[,cultivar]))
+#         flatparams<-as.list(as.numeric(dfcrop[!pasflat, cultivar])) ; names(flatparams)<-dfcrop[!pasflat, "name"]
+#         complexparams<-dfcrop[, c("name", cultivar)]
+#         stages<-unlist(strsplit(complexparams["stagelist", cultivar], split=','))
+#         descriptionstages<-list()
+#         for (sta in stages) {
+#           texte<-gsub(pattern='"', replacement="'", x=complexparams[paste("description", sta, sep="_"),cultivar])
+#           toto<-list(eval(parse(text=texte)))
+#           names(toto)<-sta
+#           descriptionstages<-c(descriptionstages, toto)
+#         }
+#         ALLCROPS[[crop]][[cultivar]]<<-c(flatparams, list(phenology=descriptionstages))
+#       }
+# 
+# 
+#     }
+#   } else stop("crop format should only standard SSM")
+# } #end read crops
+
+#' Reads in Excel crop parameter file in the same format as crops in SSM, except with extra lines at the end for filters (example crops2.xlsx)
+#'
+#' @param xlsxfile path to the excel file of crop parameters
+#' @param allvariablesfile path to the allvariables.xlsx file, with the translations of parameter names fromSSM to SSM.R (necessary because we use the SSM format, with row names different from SSM.R names)
+#' @return returns a list of parameters (one element per species.cultivar). parameters are lists (one element per module as defined in allvariables.xlsx)
+#' @examples eReadExcelCropParameters_severalsheets(xlsxfile="inputs/crops.xlsx",
+#'   allvariablesfile="allvariables.xlsx"
+#' )
+# eReadExcelCropParameters_severalsheets<-function(xlsxfile, allvariablesfile){
+#   if (!require(openxlsx)) {warning("function readExcelCropParameters needs package openxlsx"); return(list())}
+#   #read in translations of parameter names from SSM to SSM.R
+#   trad<-read.xlsx(allvariablesfile, sheet="savedEachDay")
+#   trad<-trad[trad$typeinthemodel=="CropParameter",]
+#   modules<-c(unique(trad$module[!is.na(trad$module)]), "LAI_Secondary", "DMDistribution_SeedGrowing") #don't forget to add modules that don't have specific parameters in allvariables, but that have a filter
+#   names(modules)<-modules
+#   readmodule<-function(module, data, trad, numerocolonne){
+#     toto<-trad[!is.na(trad$module) & trad$module==module,]
+#     nomsSSM<-toto$translationSSM ; names(nomsSSM)<-toto$name
+#     paramsSSM<-lapply(nomsSSM, function(param) return(data[param,numerocolonne]))
+#     types<-toto$typeR ; names(types)<-toto$name
+#     paramsSSM[types[names(paramsSSM)]=="numeric"]<-as.numeric(paramsSSM[types[names(paramsSSM)]=="numeric"])
+#     paramsSSMR<-list()
+#     if (length(data[paste(module, "filter", sep="."), numerocolonne])>0) paramsSSMR<-list(filter=gsub(pattern= "&amp;", replacement="&", fixed=TRUE, x=data[paste(module, "filter", sep="."), numerocolonne]))
+#     return(c(paramsSSMR, paramsSSM))
+#   }
+#   paramscrops<-list()
+#   onglets<-getSheetNames(xlsxfile)
+#   for(sheet in onglets) {
+#     data<-read.xlsx(xlsxfile, sheet=sheet, rowNames=TRUE)
+#     for (i in 2:ncol(data)) {
+#       paramsmanuels<-list(
+#         name=paste(colnames(data)[i], data["name",i], sep="."),
+#         thresholds=eval(parse(text=data["thresholds", i])),
+#         waterstress=list(filter=data["waterstress.filter", i])
+#       )
+#       paramsautomatiques<-lapply(modules, readmodule, data=data, trad=trad, numerocolonne=i)
+#       lu<-list(c(paramsmanuels, paramsautomatiques))
+#       names(lu)<-lu[[1]]$name
+#       paramscrops<-c(paramscrops, lu)
+#     }
+#   }
+#   return(paramscrops)
+# }
+
+#warning: now ALLCROPS is a data.frame, with crop.cultivar as rownames
+#' Reads in Excel crop parameter file in the same format as crops in SSM, except with extra lines at the end for filters (example crops2.xlsx)
+#'
+#' @param xlsxfile path to the excel file of crop parameters
+#' @param allvariablesfile path to the allvariables.xlsx file, with the translations of parameter names fromSSM to SSM.R (necessary because we use the SSM format, with row names different from SSM.R names)
+#' @return returns a data.frame, with crop.cultivar as rownames
+#' @examples eReadExcelCropParameters(xlsxfile="/Users/user/Documents/b_maison/congeMat/D4DECLIC/runSSM/input/crops.xlsx",
+#'   allvariablesfile="allvariables.xlsx"
+#' )
+eReadExcelCropParameters<-function(xlsxfile, allvariablesfile){
+  if (!require(openxlsx)) {warning("function readExcelCropParameters needs package openxlsx"); return(list())}
+  #read in translations of parameter names from SSM to SSM.R
+  trad<-read.xlsx(allvariablesfile, sheet="savedEachDay")
+  trad<-trad[trad$typeinthemodel=="CropParameter",]
+  nomsexcel<-trad$translationSSM ; names(nomsexcel)<-trad$name
+  modules<-c(unique(trad$module[!is.na(trad$module)]), "LAI_Secondary", "DMDistribution_SeedGrowing") #don't forget to add modules that don't have specific parameters in allvariables, but that have a filter
+  names(modules)<-modules
+  readmodule<-function(module, data, trad, numerocolonne){
+    toto<-trad[!is.na(trad$module) & trad$module==module,]
+    nomsSSM<-toto$translationSSM ; names(nomsSSM)<-toto$name
+    paramsSSM<-lapply(nomsSSM, function(param) return(data[param,numerocolonne]))
+    types<-toto$typeR ; names(types)<-toto$name
+    paramsSSM[types[names(paramsSSM)]=="numeric"]<-as.numeric(paramsSSM[types[names(paramsSSM)]=="numeric"])
+    paramsSSMR<-list()
+    if (length(data[paste(module, "filter", sep="."), numerocolonne])>0) paramsSSMR<-list(filter=gsub(pattern= "&amp;", replacement="&", fixed=TRUE, x=data[paste(module, "filter", sep="."), numerocolonne]))
+    return(c(paramsSSMR, paramsSSM))
+  }
+  paramscrops<-list()
+  data<-read.xlsx(xlsxfile, sheet="allcrops", rowNames=FALSE, colNames=FALSE, na.strings ="-")
+  tdata<-as.data.frame(t(as.matrix(data[3:nrow(data), 3:ncol(data)])))
+  names(tdata)<-gsub(pattern=" ", replacement="", gsub(pattern=":", replacement="", gsub(pattern="=", replacement="", data[3:nrow(data), "X1"])))
+  tdata$name<-paste(tdata$CROP, tdata$Cultivar, sep=".")
+  if (any(duplicated(names(tdata)))) stop("excel crop parameters file contains duplicated parameter names:", paste(names(tdata)[duplicated(names(tdata))], collapse=", "))
+  translations<-trad$name ; names(translations)<-trad$translationSSM
+  names(tdata)[names(tdata) %in% names(translations)]<-translations[names(tdata) [names(tdata) %in% names(translations)]]
+  if (any(duplicated(names(tdata)))) stop("allvariables.xlsx names created duplicated parameter names:", paste(names(tdata)[duplicated(names(tdata))], collapse=", "))
+  #transformation of types
+  numericparam<-intersect(trad[trad$typeR=="numeric", "name"], names(tdata))
+  #find which columns resultd in NA
+  for (v in numericparam) {
+    if (sum(is.na(tdata[,v]))<sum(is.na(as.numeric(tdata[,v])))) print(paste("parameter", nomsexcel[v], "is supposed to be numeric but contains characters in file", xlsxfile))
+  }
+  tdata[,numericparam]<-lapply(tdata[,numericparam], as.numeric)
+  
+  #evaluation of thresholds (column threshold becomes a list whith as many elements as rows in ALLCROPS, of lists with as many elements as stages in each crop)
+  tdata$thresholds<-lapply(tdata$thresholds, function(x) eval(parse(text=x)))
+  #evaluation of actionsAtStageChange  (column actionsAtStageChange becomes a list whith as many elements as rows in ALLCROPS, of lists with as many elements as actions to do in each crop)
+  tdata$actionsAtStageChange<-lapply(tdata$actionsAtStageChange, function(x) eval(parse(text=x)))
+  #replace the amp symbol by & in filters
+  tdata[,grepl(pattern=".filter", x= names(tdata), fixed=TRUE)]<-lapply(tdata[,grepl(pattern=".filter", x= names(tdata), fixed=TRUE)], function(x) gsub(pattern="&amp;", replacement="&", x=x))
+  rownames(tdata)<-tdata$name
+  return(tdata)
+}
+
+
+eReadManagement<-function(){
+  if (PARAMSIM$managformat=="standardSSM") { #read file only once and load it in the workspace
+    pathtoExcel<-normalizePath(paste(PARAMSIM$directory, "/input/managementPlans.xlsx", sep=""))
+    locations<-getSheetNames(pathtoExcel)
+    if(length(locations)>1) warning("Your managementPlans.xlsx file has several sheets, but only the first one will be used")
+    firstcol<-read.xlsx(pathtoExcel, sheet=1, colNames=FALSE, cols=1, skipEmptyRows = FALSE, skipEmptyCols = FALSE)
+    startManag<- which(firstcol$X1=="Code")
+    allmanag<-list()
+    for (i in 1:length(startManag)){
+      dfCode<-read.xlsx(pathtoExcel, sheet=1, rows=startManag[i]:(startManag[i]+1), cols=1:3)
+      dfSowing<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+2):(startManag[i]+3), cols=1:11)
+      nitrogenScenario<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+5), cols=2, colNames=FALSE)[1,1]
+      nitrogenNumber<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+6), cols=2, colNames=FALSE)[1,1]
+      nitrogenDatetype<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+7), cols=2, colNames=FALSE)[1,1]
+      nitrogendf<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+9):(startManag[i]+9+nitrogenNumber), cols=1:4)
+      names(nitrogendf)<-c("NapplNumber", "DAPorCBD", "amount", "FracVol")
+      waterLevel<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+6), cols=7, colNames=FALSE)[1,1]
+      waterScenario<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+5), cols=6, colNames=FALSE)[1,1]
+      waterNumber<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+6), cols=6, colNames=FALSE)[1,1]
+      waterDatetype<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+7), cols=6, colNames=FALSE)[1,1]
+      waterdf<-read.xlsx(pathtoExcel, sheet=1, rows=(startManag[i]+9):(startManag[i]+9+waterNumber), cols=5:6)
+      names(waterdf)<-c("DAPorCBDorDOY", "amount")
+      managementPlan<-list(dfCode, dfSowing, nitrogenScenario, nitrogenNumber, nitrogenDatetype, nitrogendf, waterLevel, waterScenario, waterNumber, waterDatetype, waterdf)
+      names(managementPlan)<-c("dfCode", "dfSowing", "nitrogenScenario", "nitrogenNumber", "nitrogenDatetype", "nitrogendf", "waterLevel", "waterScenario", "waterNumber", "waterDatetype", "waterdf")
+      toto<-list(managementPlan) ; names(toto)<-paste("row", startManag[i]-1)
+      allmanag<-c(allmanag, toto)
+    }
+    ALLMANAGEMENTS<<-allmanag
+  } else  {
+    stop("Only standard SSM format is supported for crop management data")
+  }
+}
+
